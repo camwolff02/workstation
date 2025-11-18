a51d03391bb77666ed5a6d1db5c643178ba85b3e
cam <36940948+camwolff02@users.noreply.github.com>
2025-11-17 16:19:10 -0800

    Modified dotfiles, added flatpaks

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function fish_greeting
+    fastfetch
+end
+
 alias vim nvim
+fish_add_path /home/cam/.pixi/bin
