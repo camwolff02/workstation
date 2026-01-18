5fae9b3abd71d13c2bf3eba6765c528f435691f2
Cameron Wolff <36940948+camwolff02@users.noreply.github.com>
2026-01-18 14:09:26 -0800

    modified ghostty and fish configs

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -8,4 +8,10 @@ end
 
 alias vim nvim
 set -gx NOREPLY 36940948+camwolff02@users.noreply.github.com
-fish_add_path /home/cam/.pixi/bin
+
+switch (uname)
+    case Linux
+        fish_add_path /home/cam/.pixi/bin
+    case Darwin
+        alias tailscale "/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+end
